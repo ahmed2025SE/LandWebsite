ca718b05adfb60002d0bdee66751c20dca73b0b1
ANS <ANS@DESKTOP-O2A7JH9>
2025-05-03 18:26:59 +0300

    تم اضافه خصائص كلاس الارض

--- LandWebsite/Data/Entites/Land.cs
+++ LandWebsite/Data/Entites/Land.cs
@@ -2,5 +2,9 @@
 {
     public class Land
     {
+        public Guid landId { get; set; }
+        public double area { get; set; }
+        public double price { get; set; }
+        public string location { get; set; }
     }
 }
