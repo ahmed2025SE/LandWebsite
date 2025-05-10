a9d6e57cc1ef597d39f8ba8431ca9d5b38efced5
ANS <ANS@DESKTOP-O2A7JH9>
2025-05-10 21:05:17 +0300

    تم انشاء مجلد جديد لجميع مكونات صاحب العقار و قمنا باضافة صفحه فارغه وضيفتها تعرض جميع اصحاب العقارات و انشئنا كلاس سيرفيز جديد يعطينا بيانات خام مؤقته نستخدمها في تصميم صفحه العرض

---
+++ LandWebsite/Components/Pages/OwnerComponent/OwnerDesignServices.cs
@@ -0,0 +1,52 @@
+﻿using LandWebsite.Components.Account.Pages.Manage;
+using LandWebsite.Data.Entites;
+
+namespace LandWebsite.Components.Pages.OwnerComponents
+{
+    public class OwnerDesignServices
+    {
+        public List<Owner> GetOwners()
+        {
+            return
+              [
+
+                new Owner
+                {
+                    ownerId = Guid.NewGuid(),
+                    ownerName = "ahmed",
+                    email = "ahmed@gmail.com",
+                    phone = 092888333
+                },
+
+                 new Owner
+                {
+                    ownerId = Guid.NewGuid(),
+                    ownerName = "ali",
+                    email = "ali@gmail.com",
+                    phone = 09127773
+                } ,
+                 
+                 new Owner
+                {
+                    ownerId = Guid.NewGuid(),
+                    ownerName = "krim",
+                    email = "krim@gmail.com",
+                    phone = 0934466
+                },
+                
+                 new Owner
+                {
+                    ownerId = Guid.NewGuid(),
+                    ownerName = "hmade",
+                    email = "hmade@gmail.com",
+                    phone = 02777778
+                }
+            ];
+
+
+        }
+
+    
+    }
+
+}
